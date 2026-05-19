52b8a6f3f8aec54e1364455c8e15f119fabdf2d0
ariel raymundo <ariraymundo98@gmail.com>
2026-05-19 15:24:06 -0700

    new FixedLists in main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+
 public class Main
 {
     static void main()
@@ -7,14 +9,40 @@ public class Main
         FixedList<Integer> numbers = new FixedList<>(3);
 
         numbers.add(10);
-
         numbers.add(3);
-
         numbers.add(92);
-
         numbers.add(43); // this line should fail
 
+        System.out.println();
         System.out.println(numbers.getItems().size());
         System.out.println(numbers.getItems());
+
+
+
+        FixedList<LocalDate> dates = new FixedList<>(2);
+
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+//      dates.add(15); // this line should fail (it's an int and goes over the limit)
+
+        System.out.println();
+        System.out.println(dates.getItems().size());
+        System.out.println(dates.getItems());
+
+
+
+        FixedList<String> strings = new FixedList<>(5);
+
+        strings.add("Hello");
+        strings.add("peepee poopoo");
+        strings.add("57557");
+        strings.add("www.google.com");
+        strings.add("0-0-4");
+        strings.add("skibidi");
+        strings.add("guh"); // this line should also fail
+
+        System.out.println();
+        System.out.println(strings.getItems().size());
+        System.out.println(strings.getItems());
     }
 }
